1b47f627eb56ec11a53a563febb673725241df8a
ANEESAHMAD12 <hadi4953351@gmail.com>
2024-07-08 17:35:50 +0500

    initial commit

--- app/src/main/java/com/example/myapplication12/Basic.kt
+++ app/src/main/java/com/example/myapplication12/Basic.kt
@@ -2,4 +2,36 @@ package com.example.myapplication12
 
 fun main(){
 
+
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Rock , Paper or Scissors: Please enter a Value!")
+    playerChoice = readln()
+
+    var randomNumber = (1..3).random()
+
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
+    }
+    println(computerChoice)
+    var winner = when{
+
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "player"
+        else -> "computer"
+
+
+    }
+    println("The winner is : $winner")
+
 }
