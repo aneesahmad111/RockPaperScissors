d1115ac2f1ddd79be242e9da1a77bc175cee4c94
ANEESAHMAD12 <hadi4953351@gmail.com>
2024-07-09 11:01:23 +0500

    Detail on List(Mutable & immutable ) with for Loops

--- app/src/main/java/com/example/myapplication12/Basic.kt
+++ app/src/main/java/com/example/myapplication12/Basic.kt
@@ -1,10 +1,47 @@
 package com.example.myapplication12
 
-fun main(){
+fun main() {
 
+  //  var shoppingList = listOf("Bread","Jam","Milk")
+    var shoppingList = mutableListOf("sugar","tea","milk","soap")
+    shoppingList.add("vegetables")
+
+    shoppingList.remove("milk")
+
+
+    shoppingList.add(1,"Bread")
+
+    shoppingList.add(2,"pack Milk")
+
+    println(shoppingList)
+
+    shoppingList[2] = "Milk"
+    println(shoppingList)
+
+    println(shoppingList.contains("read"))
+/*
+    for (i in shoppingList){
+        println(i)
+        if (i=="Milk"){
+            break
+        }
+    }
+
+ */
+    for (items in 0 until  shoppingList.size){
+        println(shoppingList[items])
+    }
+
+
+
+
+/*
     var d = Dog()
-    var detailOfDog = DetailOfDog("pitty",23)
+    var detailOfDog = DetailOfDog("pitty", 23)
     d.behaviour(detailOfDog)
+
+ */
+}
 
   /*
     var computerChoice = ""
@@ -38,4 +75,3 @@ fun main(){
     }
     println("The winner is : $winner")
 */
-}
